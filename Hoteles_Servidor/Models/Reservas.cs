3507efabb383f1c4b82775041df767316018c26b
kevin urbano <kevurbmac@gmail.com>
2017-09-12 18:00:35 +0200

    Cambios de campos en la entidad Reservar

--- Hoteles_Servidor/Models/Reservas.cs
+++ Hoteles_Servidor/Models/Reservas.cs
@@ -8,13 +8,17 @@ namespace Hoteles_Servidor.Models
     public class Reservas
     {
         public int Id { get; set; }
-        public String Hotel { get; set; }
-        public String FechaLlegada { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public String Telefono { get; set; }
+        public String Email { get; set; }
         public String FechaSalida { get; set; }
+        public String FechaLlegada { get; set; }
+        public String Hoteles { get; set; }
         public int Habitaciones { get; set; }
         public int Adultos { get; set; }
         public int Ninos { get; set; }
-        public double precioTotal { get; set; }
+        public double PrecioTotal { get; set; }
 
     }
 }
